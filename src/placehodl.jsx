f70828e44248da45086e27c7f348c082c8837fd8
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2018-03-03 12:08:52 +0100

    fix: makes sure re-render dont shuffle the placeholder

--- src/placehodl.jsx
+++ src/placehodl.jsx
@@ -4,35 +4,46 @@ import frand from 'fast-random';
 
 class Placehodl extends React.Component {
   generator = frand(this.props.seed);
-
   sizes = ['small', 'medium', 'large', 'xsmall', 'xlarge'];
+
+  componentDidUpdate() {
+    this.generator = frand(this.props.seed);
+  }
 
   getSize = (size = 3) => {
     const index = parseInt(this.generator.nextInt(), 10) % size;
     return this.sizes[index];
   };
 
-  getLine = (words = 3, size) =>
-    Array(words)
-      .fill(0)
-      .map(() => (
-        <div className={`react-placehodl-word ${this.getSize(size)}`} />
-      ));
+  getLine = (words = 3, size) => {
+    const { prefix } = this.props;
+    return (
+      <div className={`${prefix}-line`}>
+        {Array(words)
+          .fill(0)
+          .map(() => (
+            <div className={`${prefix}-word ${this.getSize(size)}`} />
+          ))}
+      </div>
+    );
+  };
 
-  getParagraph = (lines = 3, words, size) =>
-    Array(lines)
-      .fill(0)
-      .map(() => (
-        <div className={`react-placehodl-paragraph ${this.getSize(size)}`}>
-          {this.getLine(words)}
-        </div>
-      ));
+  getParagraph = (lines = 3, words, size) => {
+    const { prefix } = this.props;
+    return (
+      <div className={`${prefix}-paragraph`}>
+        {Array(lines)
+          .fill(0)
+          .map(() => this.getLine(words, size))}
+      </div>
+    );
+  };
 
   render() {
-    const { children, className } = this.props;
+    const { children, className, prefix } = this.props;
     const { getSize, getLine, getParagraph } = this;
     return (
-      <div className={`react-placehodl ${className}`}>
+      <div className={`${prefix} ${className || ''}`}>
         {children({ getSize, getLine, getParagraph })}
       </div>
     );
@@ -42,7 +53,12 @@ class Placehodl extends React.Component {
 Placehodl.propTypes = {
   seed: PropTypes.number,
   className: PropTypes.string,
-  children: PropTypes.func.isRequired
+  children: PropTypes.func.isRequired,
+  prefix: PropTypes.string
+};
+
+Placehodl.defaultProps = {
+  prefix: 'react-placehodl'
 };
 
 export default Placehodl;
